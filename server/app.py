43664026ab4cadfd72be0511a23689ff54625d71
Stephen Csengo <stephencsengo@gmail.com>
2023-10-04 14:44:29 -0400

    Add Marshmallow schemas for Review and User

--- server/app.py
+++ server/app.py
@@ -3,9 +3,10 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request, session
+from flask import request, session, make_response
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
+from flask_marshmallow import Marshmallow
 
 # Local imports
 from config import app, db, api
@@ -15,32 +16,81 @@ from models import User, Book, Review, Author
 
 app.secret_key = b"?w\x85Z\x08Q\xbdO\xb8\xa9\xb65Kj\xa9_"
 
+ma = Marshmallow(app)
+
+
+# Marshmallow schemas
+class ReviewSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Review
+
+    rating = ma.auto_field()
+    review = ma.auto_field()
+    user_id = ma.auto_field()
+    book_id = ma.auto_field()
+
+
+review_schema = ReviewSchema()
+many_reviews_schema = ReviewSchema(many=True)
+
+
+class UserSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = User
+
+    id = ma.auto_field()
+    username = ma.auto_field()
+
+
+user_schema = UserSchema()
+many_users_schema = UserSchema(many=True)
+
 
 # Views go here!
 class Signup(Resource):
     def post(self):
-        json = request.get_json()
+        pass
 
-        username = json.get("username")
-        password = json.get("password")
 
-        new_user = User(username=username)
-        new_user.__password_hash = password
-        import ipdb
+class Reviews(Resource):
+    def get(self):
+        reviews = Review.query.all()
+        response = make_response(
+            many_reviews_schema.dump(reviews),
+            200,
+        )
 
-        ipdb.set_trace()
-        try:
-            db.session.add(new_user)
-            db.session.commit()
-            session["user_id"] = new_user.id
+        return response
 
-            return make_response(new_user.to_dict(), 201)
+    def post(self):
+        pass
 
-        except IntegrityError:
-            return {"error": "422 Unprocessable Entity"}, 422
+
+class ReviewByID(Resource):
+    def get(self, id):
+        review = Review.query.filter_by(id=id).first()
+
+        if review:
+            return review_schema.dump(review), 200
+        else:
+            return {"message": "Review not found"}, 404
+
+
+class Users(Resource):
+    def get(self):
+        users = User.query.all()
+        response = make_response(
+            many_users_schema.dump(users),
+            200,
+        )
+
+        return response
 
 
 api.add_resource(Signup, "/signup", endpoint="signup")
+api.add_resource(Reviews, "/reviews", endpoint="reviews")
+api.add_resource(ReviewByID, "/reviews/<int:id>")
+api.add_resource(Users, "/users", endpoint="users")
 
 
 @app.route("/")
