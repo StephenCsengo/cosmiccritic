ed5f95dcbde3dc656b64903e1facaffb8213109c
Stephen Csengo <stephencsengo@gmail.com>
2023-10-05 00:49:53 -0400

    Fix issue with book.author.name causing an error on BookDetails.js

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -5,21 +5,30 @@ import { Grid, Container, Button } from "@mui/material";
 function BookDetails() {
   const { id } = useParams();
   const [book, setBook] = useState([]);
+  const [author, setAuthor] = useState(null);
   useEffect(() => {
     fetch(`/books/${id}`)
       .then((response) => response.json())
       .then((data) => {
         setBook(data);
+        setAuthor(data.author.name);
       });
   }, []);
+  console.log(author);
   return (
     <Container>
       <Grid container>
         <Grid item xs={8}>
           <h2>{book.title}</h2>
+          <p>By {author}</p>
           <p>{book.desc}</p>
+          <h3>Details</h3>
+          <p>{book.page_count} pages</p>
+          <p>Published in {book.publish_year}</p>
         </Grid>
-        <img src={book.cover_image} alt={book.title} />
+        <Grid item xs={4}>
+          <img src={book.cover_image} alt={book.title} />
+        </Grid>
       </Grid>
     </Container>
   );
